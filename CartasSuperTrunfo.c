2d03819e3c0d6d774d856437d282cbbad4e5addc
Gabriela Berger <146837299+Berger9@users.noreply.github.com>
2025-09-14 23:19:41 -0300

    Update CartasSuperTrunfo.c
    Atualizado para nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,8 @@ int main () {
     float area1 = 432.0f; // km²
     float pib1 = 98.0f;   // bilhões de reais
     int numeroPontosTuristicos1 = 26;
+    float densidadepopulacional1 = populacao1/ area1;
+    float pibpercapita1 = (pib1 * 1e9) / populacao1;
 
     // Carta 2
     char estado2[] = "Tocantins";
@@ -22,6 +24,8 @@ int main () {
     float area2 = 2.219f; // km²
     float pib2 = 32.977f;   // bilhões de reais
     int numeroPontosTuristicos2 = 15;
+    float densidadepopulacional2 = populacao2 / area2;
+    float pibpercapita2 = ( pib2* 1e9) / populacao2;
 
     // Impressão Carta 1
     printf("Carta 1:\n");
@@ -32,6 +36,8 @@ int main () {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n\n", numeroPontosTuristicos1);
+    printf("Densidade Populacional: %.2f Hab/KM²\n", densidadepopulacional1);
+    printf("PIB per Capita: %.2f Reais\n", pibpercapita1);
 
     // Impressão Carta 2
     printf("Carta 2:\n");
@@ -42,6 +48,8 @@ int main () {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", numeroPontosTuristicos2);
+    printf("Densidade Populacional: %.2f Hab/KM²\n", densidadepopulacional2);
+    printf("PIB per Capita: %.2f Reais\n", pibpercapita2);
 
     return 0;
 
