739be8440b59f379436af7a0afd8a99da4f0cf67
Gabriela Berger <146837299+Berger9@users.noreply.github.com>
2025-09-15 00:54:26 +0000

    Nivél Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,46 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+int main () {
+  // Carta 1
+    char estado1[] = "Paraná";
+    char codigo1[] = "A01";
+    char nomeCidade1[] = "Curitiba";
+    int populacao1 = 1830795;
+    float area1 = 432.0f; // km²
+    float pib1 = 98.0f;   // bilhões de reais
+    int numeroPontosTuristicos1 = 26;
 
-  // Área para entrada de dados
+    // Carta 2
+    char estado2[] = "Tocantins";
+    char codigo2[] = "B01";
+    char nomeCidade2[] = "Palmas";
+    int populacao2 = 302692;
+    float area2 = 2.219f; // km²
+    float pib2 = 32.977f;   // bilhões de reais
+    int numeroPontosTuristicos2 = 15;
 
-  // Área para exibição dos dados da cidade
+    // Impressão Carta 1
+    printf("Carta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n\n", numeroPontosTuristicos1);
 
-return 0;
-} 
+    // Impressão Carta 2
+    printf("Carta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", numeroPontosTuristicos2);
+
+    return 0;
+
+
+}
